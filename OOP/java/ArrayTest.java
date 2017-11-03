aebfdbe313df9c001b983e35e4b89c79691f6f91
bbungsang <821heejin@naver.com>
2017-11-03 16:08:20 +0900

    Array, Linked-list, Search, Recursive, Big-O 내용 및 예제 추가

---
+++ OOP/java/ArrayTest.java
@@ -0,0 +1,10 @@
+class ArrayTest {
+    public static void main(String[] args) {
+        int n = 6;
+        int k[] = new int[n];
+        for(int i = 0; i < n; i++){
+            k[i] = i+1;
+        }
+        System.out.println(k);
+    }
+}
